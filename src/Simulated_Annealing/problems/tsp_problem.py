f403a1a54e84d3340da5f097e72e2413f42dd188
Felipe Nogueira <felipe.leite23@gmail.com>
2024-09-24 23:31:45 -0300

    optimal solution TSPProblem

--- src/Simulated_Annealing/problems/tsp_problem.py
+++ src/Simulated_Annealing/problems/tsp_problem.py
@@ -1,20 +1,31 @@
 import random
+import time
+import threading
+from itertools import permutations
 
 class TSPProblem:
     def __init__(self):
-        distance_matrix = [
-            [0, 29, 20, 21],
-            [29, 0, 15, 18],
-            [20, 15, 0, 26],
-            [21, 18, 26, 0]
+        self.distance_matrix = [
+            [0, 29, 82, 46, 68, 52, 72, 42, 51, 55, 29, 74, 23, 72, 46],
+            [29, 0, 55, 46, 42, 43, 43, 23, 23, 31, 41, 51, 11, 52, 21],
+            [82, 55, 0, 68, 46, 55, 23, 43, 41, 29, 79, 21, 64, 31, 51],
+            [46, 46, 68, 0, 82, 15, 72, 31, 62, 42, 21, 51, 51, 43, 64],
+            [68, 42, 46, 82, 0, 74, 23, 52, 21, 46, 82, 58, 46, 65, 23],
+            [52, 43, 55, 15, 74, 0, 61, 23, 55, 31, 33, 37, 51, 29, 59],
+            [72, 43, 23, 72, 23, 61, 0, 42, 23, 31, 77, 37, 51, 46, 33],
+            [42, 23, 43, 31, 52, 23, 42, 0, 33, 15, 37, 33, 33, 31, 37],
+            [51, 23, 41, 62, 21, 55, 23, 33, 0, 29, 62, 46, 29, 51, 11],
+            [55, 31, 29, 42, 46, 31, 31, 15, 29, 0, 51, 21, 41, 23, 37],
+            [29, 41, 79, 21, 82, 33, 77, 37, 62, 51, 0, 65, 42, 59, 61],
+            [74, 51, 21, 51, 58, 37, 37, 33, 46, 21, 65, 0, 61, 11, 55],
+            [23, 11, 64, 51, 46, 51, 51, 33, 29, 41, 42, 61, 0, 62, 23],
+            [72, 52, 31, 43, 65, 29, 46, 31, 51, 23, 59, 11, 62, 0, 59],
+            [46, 21, 51, 64, 23, 59, 33, 37, 11, 37, 61, 55, 23, 59, 0]
         ]
-        self.distance_matrix = distance_matrix
-        self.num_cities = len(distance_matrix)
+        self.num_cities = len(self.distance_matrix)
 
     def initial_solution(self):
-        solution = list(range(self.num_cities))
-        random.shuffle(solution)
-        return solution
+        return list(range(self.num_cities))
 
     def cost(self, solution):
         total_distance = 0
@@ -29,3 +40,30 @@ class TSPProblem:
         i, j = random.sample(range(self.num_cities), 2)
         neighbor[i], neighbor[j] = neighbor[j], neighbor[i]
         return neighbor
+    
+    # Função que calcula a solução ótima para o problema na forca bruta
+    def optimal_solution(self, time_limit=60):
+        def find_optimal(start_time):
+            nonlocal best_solution, best_cost
+            best_cost = float('inf')
+            best_solution = None
+            for perm in permutations(range(self.num_cities)):
+                if time.time() - start_time > time_limit:
+                    return
+                cost = self.cost(perm)
+                if cost < best_cost:
+                    best_cost = cost
+                    best_solution = perm
+
+        best_solution = None
+        best_cost = float('inf')
+        start_time = time.time()
+        thread = threading.Thread(target=find_optimal, args=(start_time,))
+        thread.start()
+        thread.join(timeout=time_limit)
+
+        if thread.is_alive():
+            print("Tempo limite atingido. Interrompendo a busca pela solução ótima.")
+            return None, None
+
+        return best_solution, best_cost
--- src/main.py
+++ src/main.py
@@ -1,9 +1,9 @@
 from Simulated_Annealing import knapsack_dynamic
 from Simulated_Annealing.problems.knapsack_problem import KnapsackProblem
 from Simulated_Annealing.problems.tsp_problem import TSPProblem
+from Simulated_Annealing.sa_solver import SimulatedAnnealing
 import time
 import psutil
-from Simulated_Annealing.sa_solver import SimulatedAnnealing
 
 def calculate_weight(solution, weights):
     total_weight = 0
@@ -51,12 +51,12 @@ def main():
     print("Selecione o problema para executar:")
     print("1. Problema da Mochila")
     print("2. Problema do Caixeiro Viajante")
+    
     choice = input("Digite o número do problema: ")
 
     if choice not in problems:
         print("Escolha inválida.")
         return
-
     problem_class = problems[choice]
     initial_temperature = 1000
     cooling_rate = 0.95
@@ -66,24 +66,33 @@ def main():
         problem_class, initial_temperature, cooling_rate, max_iterations
     )
 
+    print("\n==================== Resultados ====================")
     print(f"Tempo médio de execução: {avg_time:.4f} segundos")
     print(f"Uso médio de memória: {avg_memory / (1024 * 1024):.4f} MB")
-    print(f"Custo médio das melhores soluções: {-avg_cost:.4f}")  # Negativo para exibir valor real
-    print("Melhores soluções encontradas:")
+    print(f"Custo médio das melhores soluções: {-avg_cost:.4f}")
+    print("\nMelhores soluções encontradas:")
     for solution, cost in zip(best_solutions, best_costs):
-        print(f"  {solution} - Custo: {-cost:.4f}")  # Negativo para exibir valor real
-    
-    # Comparar com a solução ótima para o problema da mochila
+        print(f"  {solution} - Custo: {cost}") 
+
     if choice == "1":
         problem = KnapsackProblem()
         optimal_cost = knapsack_dynamic(problem.weights, problem.values, problem.capacity)
+        print("\n==================== Comparação com Solução Ótima ====================")
         print(f"Solução ótima para o problema da mochila: {optimal_cost}")
         print(f"Melhor custo encontrado pelo Simulated Annealing: {-min(best_costs)}")  # Negativo porque a função usa -total_value
-
-        # Calcular o peso da melhor solução
         best_solution = best_solutions[best_costs.index(min(best_costs))]
         best_weight = calculate_weight(best_solution, problem.weights)
         print(f"Peso total da melhor solução: {best_weight}")
-
+    elif choice == "2":
+        problem = TSPProblem()
+        optimal_solution, optimal_cost = problem.optimal_solution(time_limit=10)
+        print("\n==================== Comparação com Solução Ótima ====================")
+        if optimal_solution is None:
+            print("Tempo limite atingido para a solução ótima.")
+            print(f"Melhor custo para P01 com 15 cidades do Caixeiro Viajante segundo https://people.sc.fsu.edu/~jburkardt/datasets/tsp/tsp.html: 291")
+        else:
+            print(f"Solução ótima para o problema do Caixeiro Viajante: {optimal_solution} - Custo: {optimal_cost}")
+        print(f"Melhor custo encontrado pelo Simulated Annealing: {min(best_costs)}")
+          
 if __name__ == "__main__":
     main()
